bb9040cf900bb5bdbab5b1b02019c66915f608b6
Ciaran Woodward <ciaranwoodward@gmail.com>
2023-10-01 02:49:31 +0100

    Wire up flower animations

--- Plants/Flower.gd
+++ Plants/Flower.gd
@@ -5,17 +5,24 @@ extends BasePlant
 @export var time_to_grow = 3
 @export var time_to_dead = 6
 
+@onready var stateMachine = $AnimationTree["parameters/playback"]
+
 static func plant_name():
 	return "Flower"
 
 static func plant_description():
 	return "Improves soil mositure for later planting."
-	
+
+func _ready():
+	scale.x = 1 if randi_range(0, 1) else -1
+	scale *= randf_range(0.9, 1.1)
+
 func plant_type():
 	return Global.PlantType.FLOWER
 
 func harvest():
-	Global.change_produce_count(Global.ProduceType.FLOWER, 1)
+	if status == Status.HARVESTABLE:
+		Global.change_produce_count(Global.ProduceType.FLOWER, 1)
 	destroy()
 
 func destroy():
@@ -26,8 +33,12 @@ func tick():
 	if status != Status.DEAD:
 		parent_tile.fertility += 2
 	if age > time_to_dead:
+		if status != Status.DEAD:
+			stateMachine.travel("Withered")
 		status = Status.DEAD
 	elif age > time_to_grow:
+		if status != Status.HARVESTABLE:
+			stateMachine.travel("Big")
 		status = Status.HARVESTABLE
 
 func can_sow(tile : PlantTile) -> bool:
