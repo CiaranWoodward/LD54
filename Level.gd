9bd98886852507e0f53b145b356ab7fd3a54aee0
Ciaran Woodward <ciaranwoodward@gmail.com>
2023-10-01 02:17:10 +0100

    Fix not getting more AP each day

--- Level.gd
+++ Level.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+@export var AP_per_day = 5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Cursor.clear_action_func = $HUD.clear_action
 	$Cursor.connect("interacted_with_tile", func(tile): $GardenTiles.ripple_from($GardenTiles.local_to_map(tile.position)))
-	Global.action_points = 5
+	Global.action_points = AP_per_day
 	Global.day = 0
 	Global.set_quota_count(Global.ProduceType.BERRY, 5)
 	Global.set_produce_count(Global.ProduceType.BERRY, 2)
@@ -29,3 +31,4 @@ func _on_turn_end():
 	$HUD.clear_action()
 	$GardenTiles.tick()
 	Global.day += 1
+	Global.action_points = AP_per_day
