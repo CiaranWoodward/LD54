10ac5c21048519fc39a02c134326c1a390a62a44
Mai-Lan Vu <miavu11@gmail.com>
2023-10-02 13:37:00 +0100

    berryvine and succulent anims

--- Plants/BerryVine.gd
+++ Plants/BerryVine.gd
@@ -7,6 +7,9 @@ static func plant_name():
 static func plant_description():
 	return "Spreads and fruits with delicious berries!"
 
+func _ready():
+	$AnimationTree.active = true
+
 func plant_type():
 	return Global.PlantType.BERRY_VINE
 	
